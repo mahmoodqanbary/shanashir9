37b17d7e96679f534ace2cb8363238f5e998bb54
Mahmood Qanbary <MahmoodQanbary@gmail.com>
2023-02-21 23:10:46 +0330

    initial

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
+
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'), 
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+    /*=============== Menu Show ===============*/
+    // Validate if constant exists
+
+    if(navToggle){
+       navToggle.addEventListener('click', () => {
+        navToggle.classList.add('hidden');
+        navMenu.classList.add('show-menu');
+       
+       }) 
     }
-})
+    
+    /*=============== Hide Menu ===============*/
+
+    if(navClose){
+        navClose.addEventListener('click', () =>{
+            navMenu.classList.remove('show-menu');
+            navToggle.classList.remove('hidden')
+        })
+    }
+
+
+
