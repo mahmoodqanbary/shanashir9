4efdb232529fdbe26403592c32ce6f024a77d7a2
Mahmood Qanbary <MahmoodQanbary@gmail.com>
2023-03-11 01:52:47 +0330

    team

--- js/main.js
+++ js/main.js
@@ -30,3 +30,25 @@ const navMenu = document.getElementById('nav-menu'),
 
 
 
+
+    // Project Slider
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
+
+
